3210a2b012dd327d6d00a0e82221b5d09617d45d
Julian Metzler <git@mezgr.de>
2025-01-03 17:59:35 +0100

    Switch to uwsgi and add auth

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2023 Julian Metzler
+Copyright (C) 2023-2025 Julian Metzler
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,26 +16,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from flask import Flask, request, render_template
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import check_password_hash
+
+from pyfis.aesys import AesysDSA
+
+from _config import *
+from local_secrets import USERS
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+dsa = AesysDSA(CONFIG_DSA_PORT)
+text = ""
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in USERS and \
+            check_password_hash(USERS.get(username), password):
+        return username
+
 
 @app.route("/", methods=["GET", "POST"])
+@auth.login_required
 def root():
+    global dsa, text
     if request.method == "POST":
-        app.config['text'] = str(request.form['text'])
-        app.config['dsa'].send_text(app.config['text'])
-    return render_template("text.html", text=app.config['text'])
+        text = str(request.form['text'])
+        dsa.send_text(text)
+    return render_template("text.html", text=text)
 
 @app.route("/api/text.json", methods=["POST"])
+@auth.login_required
 def post_text():
+    global dsa, text
     data = request.get_json()
     if 'text' not in data:
         return {'error': "No text provided"}
-    app.config['text'] = str(data['text'])
-    app.config['dsa'].send_text(app.config['text'])
+    text = str(data['text'])
+    dsa.send_text(text)
     return {'error': None}
 
 @app.route("/api/text.json", methods=["GET"])
 def get_text():
-    return {'error': None, 'text': app.config['text']}
+    global text
+    return {'error': None, 'text': text}
